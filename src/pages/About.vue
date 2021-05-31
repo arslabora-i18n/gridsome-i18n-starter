6f84e99a4407b697ca19d2ac597f289d519f0123
arslabora <dev.arslabora@gmail.com>
2021-05-31 00:47:44 -0300

    Localise titles

--- src/pages/About.vue
+++ src/pages/About.vue
@@ -1,7 +1,9 @@
 <template>
   <Layout>
-    <h1>{{ $t('pages.about') }}</h1>
+    <div class="container prose prose-lg">
+    <h1>{{ $t('titles.about-us') }}</h1>
     <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Error doloremque omnis animi, eligendi magni a voluptatum, vitae, consequuntur rerum illum odit fugit assumenda rem dolores inventore iste reprehenderit maxime! Iusto.</p>
+    </div>
   </Layout>
 </template>
 
