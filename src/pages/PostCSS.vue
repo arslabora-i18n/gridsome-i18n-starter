31f97600c61cb6bdecec3638e289728a9a310c30
spadino <spada.andrea@gmail.com>
2021-06-10 23:04:25 -0300

    Add examples code to pastcss page

--- src/pages/PostCSS.vue
+++ src/pages/PostCSS.vue
@@ -1,10 +1,12 @@
 <template>
   <Layout>
     <div class="container max-w-screen-md mx-auto">
-
       <h1>PostCSS</h1>
-      <p>We can register and use any postcss plugin we want. This boilerplate come with some plugins already configured.</p>
-      
+      <p>
+        We can register and use any postcss plugin we want. This boilerplate
+        come with some plugins already configured.
+      </p>
+
       <h2>LostGrid</h2>
 
       <article>
@@ -13,27 +15,107 @@
           <span class="bg-red-200">1</span>
           <span class="bg-green-200">2</span>
           <span class="bg-blue-200">3</span>
-          <span class="bg-yellow-200">4</span>          
+          <span class="bg-yellow-200">4</span>
         </section>
+
+        <pre>
+&lt;section&gt;
+  &lt;span class=&quot;bg-red-200&quot;&gt;1&lt;/span&gt;
+  &lt;span class=&quot;bg-green-200&quot;&gt;2&lt;/span&gt;
+  &lt;span class=&quot;bg-blue-200&quot;&gt;3&lt;/span&gt;
+  &lt;span class=&quot;bg-yellow-200&quot;&gt;4&lt;/span&gt;    
+&lt;/section&gt;
+
+section {
+    lost-utility: clearfix;
+}
+
+span {
+  lost-column: 1/2;
+}                
+</pre
+        >
 
         <h3>Controlling Cycle</h3>
         <section class="row">
           <div class="quarter">1</div>
           <div class="half">2</div>
-          <div class="quarter">3</div>          
+          <div class="quarter">3</div>
         </section>
+
+        <pre>
+&lt;section class=&quot;row&quot;&gt;
+  &lt;div class=&quot;quarter&quot;&gt;1&lt;/div&gt;
+  &lt;div class=&quot;half&quot;&gt;2&lt;/div&gt;
+  &lt;div class=&quot;quarter&quot;&gt;3&lt;/div&gt;
+&lt;/section&gt;
+
+.row {
+  lost-utility: clearfix;
+}
+
+.quarter {
+  lost-column: 1/4 0;
+  background-color: lightseagreen;
+}
+
+.half {
+  lost-column: 1/2 0;
+  background-color: lightgoldenrodyellow;
+}
+</pre
+        >
 
         <h3>Masonry</h3>
         <section class="masonry-container">
-          <div class="masonry-item"><g-image src="http://placeimg.com/480/960/nightlife" /></div>
-          <div class="masonry-item"><g-image src="http://placeimg.com/900/600/animals" /></div>
-          <div class="masonry-item"><g-image src="http://placeimg.com/720/300/sports" /></div>
-          <div class="masonry-item"><g-image src="http://placeimg.com/900/600/sports" /></div>
-          <div class="masonry-item"><g-image src="http://placeimg.com/500/500/any" /></div>
-          <div class="masonry-item"><g-image src="http://placeimg.com/400/700/any" /></div>
-          <div class="masonry-item"><g-image src="http://placeimg.com/600/900/animals" /></div>
-          <div class="masonry-item"><g-image src="http://placeimg.com/500/500/city" /></div>
+          <div class="masonry-item">
+            <g-image src="~/assets/images/cats/cat06.jpg" />
+          </div>
+          <div class="masonry-item">
+            <g-image src="~/assets/images/cats/cat01.webp" />
+          </div>
+          <div class="masonry-item">
+            <g-image src="~/assets/images/cats/cat02.jpg" />
+          </div>
+          <div class="masonry-item">
+            <g-image src="~/assets/images/cats/cat03.jpg" />
+          </div>
+          <div class="masonry-item">
+            <g-image src="~/assets/images/cats/cat04.webp" />
+          </div>
+          <div class="masonry-item">
+            <g-image src="~/assets/images/cats/cat08.png" />
+          </div>
+          <div class="masonry-item">
+            <g-image src="~/assets/images/cats/cat07.jpg" />
+          </div>
+          <div class="masonry-item">
+            <g-image src="~/assets/images/cats/cat05.jpg" />
+          </div>
         </section>
+
+        <pre>
+&lt;section class=&quot;masonry-container&quot;&gt;
+        &lt;div class=&quot;masonry-item&quot;&gt;
+          &lt;g-image src=&quot;~/assets/images/cats/cat06.jpg&quot; /&gt;
+        &lt;/div&gt;
+        &lt;div class=&quot;masonry-item&quot;&gt;
+          &lt;g-image src=&quot;~/assets/images/cats/cat01.webp&quot; /&gt;
+        &lt;/div&gt;
+        &lt;div class=&quot;masonry-item&quot;&gt;
+          &lt;g-image src=&quot;~/assets/images/cats/cat08.png&quot; /&gt;
+        &lt;/div&gt;
+        ...
+
+.masonry-container {
+  lost-masonry-wrap: no-flex;
+}
+
+.masonry-item {
+  lost-masonry-column: 1/2;
+}
+</pre
+        >
       </article>
     </div>
   </Layout>
--- tailwind.config.js
+++ tailwind.config.js
@@ -2,27 +2,25 @@ module.exports = {
   jit: true,
   purge: {
     content: [
-      './src/**/*.vue',
-      './src/**/*.js',
-      './src/**/*.jsx',
-      './src/**/*.html',
-      './src/**/*.pug',
-      './src/**/*.md'
+      "./src/**/*.vue",
+      "./src/**/*.js",
+      "./src/**/*.jsx",
+      "./src/**/*.html",
+      "./src/**/*.pug",
+      "./src/**/*.md"
     ]
   },
   darkMode: false, // or 'media' or 'class'
   theme: {
     extend: {
       fontFamily: {
-        'atma': ['Atma', 'serif'],
-        'lato': ['Lato', 'sans-serif']
+        atma: ["Atma", "serif"],
+        lato: ["Lato", "sans-serif"]
       }
-    },
+    }
   },
   variants: {
-    extend: {},
+    extend: {}
   },
-  plugins: [
-    require('@tailwindcss/typography'),
-  ],
-}
+  plugins: [require("@tailwindcss/typography")]
+};
